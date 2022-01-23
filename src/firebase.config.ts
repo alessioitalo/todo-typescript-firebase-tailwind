356a4609b83ff7dd3e6d3b9cc43397e03445db33
Alessio Italo Junior D'Alise <Alessioitalo@gmail.com>
2022-01-23 12:14:40 +0000

    initialize firebase

---
+++ src/firebase.config.ts
@@ -0,0 +1,18 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyBT2h2uehAyL-Y1qY8FlpLrFrFw5nnVe3k',
+  authDomain: 'todo-ts-bae9a.firebaseapp.com',
+  projectId: 'todo-ts-bae9a',
+  storageBucket: 'todo-ts-bae9a.appspot.com',
+  messagingSenderId: '530845751993',
+  appId: '1:530845751993:web:1dfdb371b7b405ec5c9968',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app)
+export const db = getFirestore(app);
