8665955c33f5e624a240d43bff437d8d00e4caed
Alessio Italo Junior D'Alise <Alessioitalo@gmail.com>
2022-01-24 17:43:38 +0000

    fetching todos from firestore

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -2,21 +2,24 @@ import { collection, getDocs, query, where } from 'firebase/firestore';
 import { db } from '../firebase.config';
 import { useEffect, useState } from 'react';
 
-interface TodosProps {
+interface todosProps {
   uid: string;
 }
 
-const Todos = ({ uid }: TodosProps) => {
-  const [todos, setTodos] = useState([{ id: '', data: {} }]);
+const Todos = ({ uid }: todosProps) => {
+
+  const [todos, setTodos] = useState([
+    { id: '', data: { user: '', todo: '' } },
+  ]);
 
   useEffect(() => {
-    let todosArray: { id: string; data: {} }[];
+    let todosArray: any[] = [];
     const fetchTodos = async () => {
       const todosRef = collection(db, 'todos');
       const q = query(todosRef, where('user', '==', uid));
       const snapshot = await getDocs(q);
       snapshot.forEach((doc) => {
-        return todos.push({
+        return todosArray.push({
           id: doc.id,
           data: doc.data(),
         });
@@ -25,14 +28,19 @@ const Todos = ({ uid }: TodosProps) => {
     };
 
     fetchTodos();
-    console.log(todos)
-
-  });
+  }, [uid]);
 
   return (
-    <>
-      <div className='bg-white border-2 border-black rounded w-100 my-6 mx-10 h-12'></div>
-    </>
+    <div className='my-6'>
+      {todos.map((todo) => (
+        <div
+          className='bg-white border-2 border-black rounded w-100 my-1 mx-10 px-6 h-12 flex items-center'
+          key={todo.id}
+        >
+          {todo.data.todo}
+        </div>
+      ))}
+    </div>
   );
 };
 
